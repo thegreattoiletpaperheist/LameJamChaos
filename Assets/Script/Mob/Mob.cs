4cb251c32a38e67bec2e376952419b5fe314244c
Egor Bashinsky <nelzeabeztak@gmail.com>
2024-09-21 19:25:10 +0300

    dumb emission

--- Assets/Script/Mob/Mob.cs
+++ Assets/Script/Mob/Mob.cs
@@ -1,14 +1,23 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Mob : MonoBehaviour
 {
     public bool alive = true;
 
+    public GameObject DieEffect;
+
     public void Died()
     {
         if (!alive)
             return;
-    
+
+        if (DieEffect)
+        {
+            var ps = Instantiate(DieEffect, transform.position, Quaternion.Euler(0, 180, 0));
+            Destroy(ps, 5);
+        }    
+            
         Destroy(gameObject);
         Debug.Log("Me die");
     }
