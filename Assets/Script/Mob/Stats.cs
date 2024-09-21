3a1e88c7a0c0b10d8ae34b8521a46d9e090a9fc4
Egor Bashinsky <nelzeabeztak@gmail.com>
2024-09-21 22:10:41 +0300

    page

--- Assets/Script/Mob/Stats.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Stats : MonoBehaviour
-{
-  public float Speed = 1;
-  public float DamageOnCollision = 1;
-}
