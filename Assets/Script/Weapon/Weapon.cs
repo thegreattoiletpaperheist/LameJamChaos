c035e917d7e27a65b9fc232e3085ee077e5cda46
Egor Bashinsky <nelzeabeztak@gmail.com>
2024-09-21 17:48:51 +0300

    bullets per minute

--- Assets/Script/Weapon/Weapon.cs
+++ Assets/Script/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -5,6 +6,11 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bullet;
     private Transform BulletHolder;
+
+    public float BulletsPerMinute = 60;
+    private float lastShotFired = 0;
+
+    public bool CanShoot;
     
     private void Start()
     {
@@ -13,11 +19,21 @@ public class Weapon : MonoBehaviour
     
     public void Emit(Vector3 direction)
     {
+        if(!CanShoot)
+            return;
+        
         var b = Instantiate(bullet, transform.position, quaternion.identity,BulletHolder);
         b.transform.localScale *=  0.25f;
         var dir = b.AddComponent<GoToDirection>();
         dir.direction = direction;
 
         Destroy(b, 2);
+        
+        lastShotFired = Time.time;
+    }
+    
+    public void Update()
+    {
+        CanShoot = (Time.time - lastShotFired) > ( 60 / BulletsPerMinute);
     }
 }
